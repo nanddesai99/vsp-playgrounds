124461312e7fc9777f48ab8b5602c27891e5926b
dziemke <ziemke@vsp.tu-berlin.de>
2018-10-19 12:20:13 +0200

    use more flexible naming of density facilites

--- dziemke/src/main/java/playground/dziemke/accessibility/AccessibilityComputationBerlin_V2.java
+++ dziemke/src/main/java/playground/dziemke/accessibility/AccessibilityComputationBerlin_V2.java
@@ -29,6 +29,7 @@ import org.matsim.contrib.accessibility.AccessibilityConfigGroup;
 import org.matsim.contrib.accessibility.AccessibilityConfigGroup.AreaOfAccesssibilityComputation;
 import org.matsim.contrib.accessibility.AccessibilityModule;
 import org.matsim.contrib.accessibility.FacilityTypes;
+import org.matsim.contrib.accessibility.Labels;
 import org.matsim.contrib.accessibility.Modes4Accessibility;
 import org.matsim.contrib.accessibility.utils.AccessibilityUtils;
 import org.matsim.contrib.accessibility.utils.VisualizationUtils;
@@ -170,7 +171,7 @@ public class AccessibilityComputationBerlin_V2 {
 		final List<String> activityTypes = Arrays.asList(new String[]{FacilityTypes.EDUCATION});
 //		final List<String> activityTypes = Arrays.asList(new String[]{"s"});
 		
-		final ActivityFacilities densityFacilities = AccessibilityUtils.createFacilityForEachLink(scenario.getNetwork()); // will be aggregated in downstream code!
+		final ActivityFacilities densityFacilities = AccessibilityUtils.createFacilityForEachLink(Labels.POPULATION_DENSITIY, scenario.getNetwork()); // will be aggregated in downstream code!
 		
 		final Controler controler = new Controler(scenario);
 		
--- dziemke/src/main/java/playground/dziemke/accessibility/AccessibilityComputationNairobiMatrixBased.java
+++ dziemke/src/main/java/playground/dziemke/accessibility/AccessibilityComputationNairobiMatrixBased.java
@@ -28,6 +28,7 @@ import org.matsim.contrib.accessibility.AccessibilityConfigGroup;
 import org.matsim.contrib.accessibility.AccessibilityConfigGroup.AreaOfAccesssibilityComputation;
 import org.matsim.contrib.accessibility.AccessibilityModule;
 import org.matsim.contrib.accessibility.FacilityTypes;
+import org.matsim.contrib.accessibility.Labels;
 import org.matsim.contrib.accessibility.Modes4Accessibility;
 import org.matsim.contrib.accessibility.utils.AccessibilityUtils;
 import org.matsim.contrib.accessibility.utils.VisualizationUtils;
@@ -100,7 +101,7 @@ public class AccessibilityComputationNairobiMatrixBased {
 		
 		final List<String> activityTypes = Arrays.asList(new String[]{FacilityTypes.WORK});
 
-		final ActivityFacilities densityFacilities = AccessibilityUtils.createFacilityForEachLink(scenario.getNetwork());
+		final ActivityFacilities densityFacilities = AccessibilityUtils.createFacilityForEachLink(Labels.POPULATION_DENSITIY, scenario.getNetwork());
 
 		final Controler controler = new Controler(scenario);
 
